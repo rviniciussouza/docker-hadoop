cdad981ea665e9355adbcfd73ef77a2a3f1f78e2
Vinícius Rodrigues <vrs2@aluno.ifnmg.edu.br>
2021-08-10 04:23:10 -0300

    Adicionando opção de printar resultado no console

--- application/run.sh
+++ application/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Arquivos de entrada
+echo "Copiando arquivos de entrada para o HDFS..."
 $HADOOP_HOME/bin/hdfs dfs -put -f $PATH_DATASET /datasets/
 
 # Formatando o diretório de saída
@@ -11,6 +12,9 @@ echo "Submetendo o JOB"
 $HADOOP_HOME/bin/hadoop jar $PATH_TO_JAR $CLASSNAME $INPUT_DFS $OUTPUT_DFS
 
 # Print
+if [ $PRINT_RESULT == 1 ]; then    
+    $HADOOP_HOME/bin/hdfs dfs -cat /output/*
+if
+
 echo "Copiando arquivos de saída para o sistema de arquivos local"
-$HADOOP_HOME/bin/hdfs dfs -cat /output/*
-$HADOOP_HOME/bin/hdfs dfs -copyToLocal -f /output data/ 
+$HADOOP_HOME/bin/hdfs dfs -copyToLocal -f /output/ data/outputs/${CLASSNAME}_$(date +%Y%m%d%H%M%S)/
